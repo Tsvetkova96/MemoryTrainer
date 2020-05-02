ad919d5d2a7ffa7fd6a0ec0dd9f302fbc782763c
tsvetkova <tsvetkova96@mail.ru>
2020-05-02 11:15:05 +0300

    Добавлено создание фрейма в который будет выводиться интерфейс приложения

--- src/com/codebind/Main.java
+++ src/com/codebind/Main.java
@@ -1,4 +1,15 @@
 package com.codebind;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(1000, 800));
+        frame.setVisible(true);
+    }
 }
