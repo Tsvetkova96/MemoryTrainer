8dc7ac0448422290eb4421588d09591603cbcfef
tsvetkova <tsvetkova96@mail.ru>
2020-05-02 11:37:23 +0300

    Добавлен класс Trainer для управления процессом тренировки

---
+++ src/com/codebind/Classes/Trainer.java
@@ -0,0 +1,28 @@
+package com.codebind.Classes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Trainer {
+    ArrayList<String> ShulteTable;
+    int currentIndex;
+    public Trainer(int size) {
+        ShulteTable = new ArrayList<>();
+        for (int i = 1; i <= size * size; ++i) {
+            ShulteTable.add(String.valueOf(i));
+        }
+        Collections.shuffle(ShulteTable);
+        currentIndex = 0;
+    }
+    public  boolean checkSymbol(String checkingSymbol) {
+        String currentSymbol = ShulteTable.get(currentIndex);
+        if(currentSymbol.equals(checkingSymbol)){
+            currentIndex++;
+            return  true;
+        }
+        return  false;
+    }
+    public boolean CheckEndTrainig() {
+        return currentIndex == ShulteTable.size();
+    }
+}
